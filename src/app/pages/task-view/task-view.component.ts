e280ff6dc3ed1d93e01c7f3505d55d23ca2f2ae4
MarianChoma <xchoma@stuba.sk>
2021-10-23 00:30:19 +0200

    First commit

---
+++ src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {TaskService} from "../../task.service";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Task} from "../../models/task.model";
+import {List} from "../../models/list.model";
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+
+  lists: any;
+  tasks: any;
+
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+      console.log(params);
+        this.taskService.getTasks(params.listId).subscribe((tasks: any)=>{
+          this.tasks=tasks;
+        });
+    }
+    )
+    this.taskService.getLists().subscribe((lists: any)=>{
+        this.lists=lists;
+    });
+  }
+  onTaskClick(task: any){
+   this.taskService.complete(task).subscribe(() =>{
+      console.log("Completted");
+      task.completed= !task.completed
+    });
+  }
+}
